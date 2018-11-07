6bf1824a32743db144c22c008f75ce19fb97f219
micmcgrorty <37194621+micmcgrorty@users.noreply.github.com>
2018-11-07 18:59:38 +0000

    Added different search parameters

--- main.js
+++ main.js
@@ -4,14 +4,17 @@ let api = 'https://api.punkapi.com/v2/beers';
 let query = document.getElementById('query');
 let random = document.getElementById('random');
 let search = document.getElementById('search');
+let searchtype = document.getElementById('searchtype');
 let beerdetails = document.getElementById('beerdetails');
 
 random.addEventListener('click', function(){
+    beerdetails.innerHTML = '';
     request(api + '/random');
 })
 
 search.addEventListener('click', function(){
-    request(api + '?beer_name=' + query.value);
+    beerdetails.innerHTML = '';
+    request(api + searchtype.value + query.value);
 })
 
 function request(api) {
@@ -19,14 +22,19 @@ function request(api) {
     .then(function(response) {
         return response.json();
     })
-    .then(function(beer) {
-        let name = beer[0].name;
-        let img = beer[0].image_url;
-        let tagline = beer[0].tagline;
-        console.log(beer);
-
-        beerdetails.innerHTML = '<h4>' + name + '</h4>';
-        beerdetails.innerHTML += '<h5>' + tagline + '</h5>';
-        beerdetails.innerHTML += '<img src="' + img + '">';
+    .then(function(beers) {
+        if (beers.statusCode != 400) {
+            beers.forEach(function(beer){
+                let name = beer.name;
+                let img = beer.image_url;
+                let tagline = beer.tagline;
+    
+                beerdetails.innerHTML += '<h4>' + name + '</h4>';
+                beerdetails.innerHTML += '<h5>' + tagline + '</h5>';
+                beerdetails.innerHTML += '<img src="' + img + '">'; 
+            })
+        } else {
+            beerdetails.innerHTML = '<h4>No beers found</h4>';
+        }
     });
 }
